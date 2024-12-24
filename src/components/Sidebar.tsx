acafa4d8bd924c4a2bf48100a4042c2707d6024a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-24 10:47:22 -0300

    initial commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { PencilLine } from '@phosphor-icons/react';
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+	return (
+		<aside className={styles.sidebar}>
+			<img
+				className={styles.cover}
+				src="https://images.unsplash.com/photo-1672957581550-6b37dcdbf6ff?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			/>
+
+			<div className={styles.profile}>
+				<Avatar src="https://github.com/victorbadaro.png" />
+				<strong>Victor Badaró</strong>
+				<span>Web Developer</span>
+			</div>
+
+			<footer>
+				<a href="#">
+					<PencilLine size={20} />
+					Editar seu perfil
+				</a>
+			</footer>
+		</aside>
+	);
+}
